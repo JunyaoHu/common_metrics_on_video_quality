54ac608fad149791064b817e0adf9de2daab9b41
胡钧耀 <06192081@cumt.edu.cn>
2023-02-07 13:59:32 +0800

    Update calculate_psnr.py

--- calculate_psnr.py
+++ calculate_psnr.py
@@ -3,7 +3,7 @@ import torch
 from tqdm import tqdm
 import math
 
-def psnr(img1, img2):
+def img_psnr(img1, img2):
     # [0,1]
     # compute mse
     # mse = np.mean((img1-img2)**2)
@@ -11,23 +11,25 @@ def psnr(img1, img2):
     # compute psnr
     if mse < 1e-10:
         return 100
-    psnr = 20 * math.log10(255 / math.sqrt(mse))
+    psnr = 20 * math.log10(1 / math.sqrt(mse))
     return psnr
 
-def calculate_psnr(batches1, batches2, calculate_per_frame):
+def trans(x):
+    return x
+
+def calculate_psnr(videos1, videos2, calculate_per_frame):
     print("calculate_psnr...")
 
-    # batches [batch_num, batch_size, timestamps, channel, h, w]
+    # videos [batch_size, timestamps, channel, h, w]
     
-    assert batches1.shape == batches2.shape
+    assert videos1.shape == videos2.shape
 
-    # videos [batch_num * batch_size, timestamps, channel, h, w]
-    videos1 = torch.flatten(batches1, start_dim=0, end_dim=1)
-    videos2 = torch.flatten(batches2, start_dim=0, end_dim=1)
+    videos1 = trans(videos1)
+    videos2 = trans(videos2)
     
     psnr_results = []
     
-    for video_num in tqdm(range(len(videos1))):
+    for video_num in tqdm(range(videos1.shape[0])):
         # get a video
         # video [timestamps, channel, h, w]
         video1 = videos1[video_num]
@@ -37,21 +39,29 @@ def calculate_psnr(batches1, batches2, calculate_per_frame):
         for clip_timestamp in range(calculate_per_frame, len(video1)+1, calculate_per_frame):
             # get a img
             # img [timestamps[x], channel, h, w]
-            # img [channel, h, w] tensor
+            # img [channel, h, w] numpy
 
             img1 = video1[clip_timestamp-1].numpy()
             img2 = video2[clip_timestamp-1].numpy()
             
             # calculate psnr of a video
-            psnr_results_of_a_video.append(psnr(img1, img2))
+            psnr_results_of_a_video.append(img_psnr(img1, img2))
 
         psnr_results.append(psnr_results_of_a_video)
+    
+    psnr = {}
+    psnr_std = {}
+
+    for idx, clip_timestamp in enumerate(range(calculate_per_frame, len(video1)+1, calculate_per_frame)):
+        psnr[clip_timestamp] = np.mean(psnr_results, axis=0)[idx]
+        psnr_std[clip_timestamp] = np.std(psnr_results, axis=0)[idx]
 
     result = {
-        "psnr_video_setting": batches1.shape,
+        "psnr": psnr,
+        "psnr_std": psnr_std,
         "psnr_per_frame": calculate_per_frame,
-        "psnr_mean_per_frame": np.mean(psnr_results, axis=0),
-        "psnr_std_per_frame": np.std(psnr_results, axis=0)
+        "psnr_video_setting": video1.shape,
+        "psnr_video_setting_name": "time, channel, heigth, width",
     }
 
     return result
@@ -59,15 +69,19 @@ def calculate_psnr(batches1, batches2, calculate_per_frame):
 # test code / using example
 
 def main():
-    NUMBER_OF_BATCHES = 4
     NUMBER_OF_VIDEOS = 8
     VIDEO_LENGTH = 30
     CHANNEL = 3
     SIZE = 64
-    CALCULATE_PER_FRAME = 1
-    batches1 = torch.ones(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
-    batches2 = torch.ones(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
-    print(calculate_psnr(batches1, batches2, CALCULATE_PER_FRAME))
+    CALCULATE_PER_FRAME = 10
+    videos1 = torch.zeros(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    videos2 = torch.zeros(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    device = torch.device("cuda")
+
+    import json
+    result = calculate_psnr(videos1, videos2, CALCULATE_PER_FRAME)
+    print(json.dumps(result, indent=4))
+
 
 if __name__ == "__main__":
     main()
