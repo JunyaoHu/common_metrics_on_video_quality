83970db0555af8c4c6215903797e22aa260ff96d
胡钧耀 <06192081@cumt.edu.cn>
2023-02-07 13:59:14 +0800

    Update calculate_lpips.py

--- calculate_lpips.py
+++ calculate_lpips.py
@@ -12,22 +12,32 @@ spatial = True         # Return a spatial map of perceptual distance.
 loss_fn = lpips.LPIPS(net='alex', spatial=spatial) # Can also set net = 'squeeze' or 'vgg'
 # loss_fn = lpips.LPIPS(net='alex', spatial=spatial, lpips=False) # Can also set net = 'squeeze' or 'vgg'
 
-def calculate_lpips(batches1, batches2, calculate_per_frame, device):
-    # image should be RGB, IMPORTANT: normalized to [-1,1] , need (1,3,64,64)
+def trans(x):
+    # if greyscale images add channel
+    if x.shape[-3] == 1:
+        x = x.repeat(1, 1, 3, 1, 1)
+
+    # value range [0, 1] -> [-1, 1]
+    x = x * 2 - 1
+
+    return x
+
+def calculate_lpips(videos1, videos2, calculate_per_frame, device):
+    # image should be RGB, IMPORTANT: normalized to [-1,1]
     print("calculate_lpips...")
 
-    # batches [batch_num, batch_size, timestamps, channel, h, w]
-    
-    assert batches1.shape == batches2.shape
+    assert videos1.shape == videos2.shape
 
-    # videos [batch_num * batch_size, timestamps, channel, h, w]
-    # value [0, 1] -> [-1, 1]
-    videos1 = torch.flatten(batches1, start_dim=0, end_dim=1) * 2 - 1
-    videos2 = torch.flatten(batches2, start_dim=0, end_dim=1) * 2 - 1
+    # videos [batch_size, timestamps, channel, h, w]
+
+    # support grayscale input, if grayscale -> channel*3
+    # value range [0, 1] -> [-1, 1]
+    videos1 = trans(videos1)
+    videos2 = trans(videos2)
     
     lpips_results = []
-    
-    for video_num in tqdm(range(len(videos1))):
+
+    for video_num in tqdm(range(videos1.shape[0])):
         # get a video
         # video [timestamps, channel, h, w]
         video1 = videos1[video_num]
@@ -46,14 +56,21 @@ def calculate_lpips(batches1, batches2, calculate_per_frame, device):
 
             # calculate lpips of a video
             lpips_results_of_a_video.append(loss_fn.forward(img1, img2).mean().detach().cpu().tolist())
-
         lpips_results.append(lpips_results_of_a_video)
+    
+    lpips_mean = {}
+    lpips_std = {}
+
+    for idx, clip_timestamp in enumerate(range(calculate_per_frame, len(video1)+1, calculate_per_frame)):
+        lpips_mean[clip_timestamp] = np.mean(lpips_results, axis=0)[idx]
+        lpips_std[clip_timestamp] = np.std(lpips_results, axis=0)[idx]
 
     result = {
-        "lpips_video_setting": batches1.shape,
+        "lpips": lpips_mean,
+        "lpips_std": lpips_std,
         "lpips_per_frame": calculate_per_frame,
-        "lpips_mean_per_frame": np.mean(lpips_results, axis=0),
-        "lpips_std_per_frame": np.std(lpips_results, axis=0)
+        "lpips_video_setting": video1.shape,
+        "lpips_video_setting_name": "time, channel, heigth, width",
     }
 
     return result
@@ -61,16 +78,18 @@ def calculate_lpips(batches1, batches2, calculate_per_frame, device):
 # test code / using example
 
 def main():
-    NUMBER_OF_BATCHES = 4
     NUMBER_OF_VIDEOS = 8
     VIDEO_LENGTH = 30
     CHANNEL = 3
     SIZE = 64
     CALCULATE_PER_FRAME = 10
-    batches1 = torch.ones(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
-    batches2 = torch.zeros(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    videos1 = torch.zeros(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    videos2 = torch.ones(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
     device = torch.device("cuda")
-    print(calculate_lpips(batches1, batches2, CALCULATE_PER_FRAME, device))
+
+    import json
+    result = calculate_lpips(videos1, videos2, CALCULATE_PER_FRAME, device)
+    print(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
     main()
