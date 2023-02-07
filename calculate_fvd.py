6fc664579bea5832bd1488526dad3eb4a0c5f2ed
胡钧耀 <06192081@cumt.edu.cn>
2023-02-07 13:58:53 +0800

    Update calculate_fvd.py

--- calculate_fvd.py
+++ calculate_fvd.py
@@ -1,50 +1,57 @@
-from models.fvd.fvd import get_fvd_feats, frechet_distance, load_i3d_pretrained, to_i3d
+from models.fvd.fvd import get_fvd_feats, frechet_distance, load_i3d_pretrained
 import numpy as np
 import torch
 from tqdm import tqdm
 
-def calculate_fvd(batches1, batches2, calculate_per_frame, device):
+def trans(x):
+    # if greyscale images add channel
+    if x.shape[-3] == 1:
+        x = x.repeat(1, 1, 3, 1, 1)
+
+    # permute BTCHW -> BCTHW
+    x = x.permute(0, 2, 1, 3, 4) 
+
+    return x
+
+def calculate_fvd(videos1, videos2, calculate_per_frame, device):
     print("calculate_fvd...")
 
-    # batches [batch_num, batch_size, timestamps, channel, h, w]
+    # videos [batch_size, timestamps, channel, h, w]
     
-    assert batches1.shape == batches2.shape
-    
+    assert videos1.shape == videos2.shape
+
     i3d = load_i3d_pretrained(device=device)
     fvd_results = []
 
-    for batch_num in tqdm(range(len(batches1))):
-        # get a batch
-        # videos [batch_size, timestamps, channel, h, w]
-        videos1 = batches1[batch_num]
-        videos2 = batches2[batch_num]
+    # support grayscale input, if grayscale -> channel*3
+    # BTCHW -> BCTHW
+    # videos -> [batch_size, channel, timestamps, h, w]
 
-        # BTCHW -> BCTHW
-        # videos [batch_size, channel, timestamps, h, w]
-        videos1 = to_i3d(videos1, videos1.shape[-3], videos1.shape[-2], videos1.shape[-1])
-        videos2 = to_i3d(videos2, videos2.shape[-3], videos2.shape[-2], videos2.shape[-1])
-        fvd_results_of_a_batch = []
+    videos1 = trans(videos1)
+    videos2 = trans(videos2)
 
-        assert calculate_per_frame >= 10
+    fvd_results = {}
 
-        for clip_timestamp in range(calculate_per_frame, videos1.shape[-3]+1, calculate_per_frame):
-            # get a video clip
-            # videos_clip [batch_size, channel, timestamps[:], h, w]
-            videos_clip1 = videos1[:, :, : clip_timestamp]
-            videos_clip2 = videos2[:, :, : clip_timestamp]
+    assert calculate_per_frame >= 10
 
-            # get FVD features
-            feats1 = get_fvd_feats(videos_clip1, i3d=i3d, device=device)
-            feats2 = get_fvd_feats(videos_clip2, i3d=i3d, device=device)
-            # calculate FVD of a batch
-            fvd_results_of_a_batch.append(frechet_distance(feats1, feats2))
-        fvd_results.append(fvd_results_of_a_batch)
+    for clip_timestamp in tqdm(range(calculate_per_frame, videos1.shape[-3]+1, calculate_per_frame)):
+        # get a video clip
+        # videos_clip [batch_size, channel, timestamps[:clip], h, w]
+        videos_clip1 = videos1[:, :, : clip_timestamp]
+        videos_clip2 = videos2[:, :, : clip_timestamp]
+
+        # get FVD features
+        feats1 = get_fvd_feats(videos_clip1, i3d=i3d, device=device)
+        feats2 = get_fvd_feats(videos_clip2, i3d=i3d, device=device)
+        
+        # calculate FVD when timestamps[:clip]
+        fvd_results[clip_timestamp] = frechet_distance(feats1, feats2)
 
     result = {
-        "fvd_video_setting": batches1.shape,
+        "fvd": fvd_results,
         "fvd_per_frame": calculate_per_frame,
-        "fvd_mean_per_frame": np.mean(fvd_results, axis=0),
-        "fvd_std_per_frame": np.std(fvd_results, axis=0)
+        "fvd_video_setting": videos1.shape,
+        "fvd_video_setting_name": "batch_size, channel, time, heigth, width",
     }
 
     return result
@@ -52,16 +59,18 @@ def calculate_fvd(batches1, batches2, calculate_per_frame, device):
 # test code / using example
 
 def main():
-    NUMBER_OF_BATCHES = 4
     NUMBER_OF_VIDEOS = 8
     VIDEO_LENGTH = 30
     CHANNEL = 3
     SIZE = 64
-    CALCULATE_PER_FRAME = 15
-    batches1 = torch.zeros(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
-    batches2 = torch.ones(NUMBER_OF_BATCHES, NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    CALCULATE_PER_FRAME = 10
+    videos1 = torch.zeros(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
+    videos2 = torch.ones(NUMBER_OF_VIDEOS, VIDEO_LENGTH, CHANNEL, SIZE, SIZE, requires_grad=False)
     device = torch.device("cuda")
-    print(calculate_fvd(batches1, batches2, CALCULATE_PER_FRAME, device))
+
+    import json
+    result = calculate_fvd(videos1, videos2, CALCULATE_PER_FRAME, device)
+    print(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
     main()
