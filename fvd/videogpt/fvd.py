dcf7e4a1d9103a19600d174f9a41a2780e40bf5d
胡钧耀 <hujunyao@mail.nankai.edu.cn>
2023-10-29 19:29:23 +0800

    Update fvd.py

--- fvd/videogpt/fvd.py
+++ fvd/videogpt/fvd.py
@@ -130,6 +130,7 @@ def get_logits(i3d, videos, device, bs=10):
         logits = []
         for i in range(0, videos.shape[0], bs):
             batch = videos[i:i + bs].to(device)
-            logits.append(i3d.module.extract_features(batch))
+            # logits.append(i3d.module.extract_features(batch)) # wrong
+            logits.append(i3d(batch)) # right
         logits = torch.cat(logits, dim=0)
         return logits
